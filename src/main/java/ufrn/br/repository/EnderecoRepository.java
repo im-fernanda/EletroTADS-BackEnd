df8207a21447757178430c99c49db909e3db1d54
Fernanda <gilneidefernandaf@gmail.com>
2024-08-14 19:23:42 -0300

    Commit inicial

--- src/main/java/ufrn/br/repository/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package ufrn.br.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ufrn.br.domain.Endereco;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
