e83a791d632dd38e4b01e9985d4926c32947c5c1
Fernanda <gilneidefernandaf@gmail.com>
2024-08-16 22:30:01 -0300

    Editando os DTO

--- src/main/java/ufrn/br/dto/CategoriaDTO.java
+++ src/main/java/ufrn/br/dto/CategoriaDTO.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoriaDTO {
     private String nome;
 }
--- src/main/java/ufrn/br/dto/ProdutoRequestDTO.java
+++ src/main/java/ufrn/br/dto/ProdutoRequestDTO.java
@@ -1,8 +1,12 @@
 package ufrn.br.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProdutoRequestDTO {
 
     private String nome;
