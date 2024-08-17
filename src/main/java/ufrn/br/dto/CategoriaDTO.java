ec4be128b282bcf9947405fb0a996e28c3578432
Fernanda <gilneidefernandaf@gmail.com>
2024-08-16 21:12:29 -0300

    Arrumando produtocategoria

--- src/main/java/ufrn/br/dto/CategoriaDTO.java
+++ src/main/java/ufrn/br/dto/CategoriaDTO.java
@@ -4,11 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class CategoriaDTO {
-    private Integer id;
     private String nome;
 }
 
