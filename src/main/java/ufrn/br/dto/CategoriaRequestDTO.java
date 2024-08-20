9e47630392f9c1b17995e95fa7b0dcf0e879740d
Fernanda <gilneidefernandaf@gmail.com>
2024-08-20 16:40:38 -0300

    Atualizando alguns DTO requests

--- src/main/java/ufrn/br/dto/CategoriaRequestDTO.java
+++ src/main/java/ufrn/br/dto/CategoriaRequestDTO.java
@@ -1,10 +1,13 @@
 package ufrn.br.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoriaRequestDTO {
     String nome;
     List<ProdutoRequestDTO> produtos;
--- src/main/java/ufrn/br/dto/ProdutoRequestDTO.java
+++ src/main/java/ufrn/br/dto/ProdutoRequestDTO.java
@@ -1,32 +1,19 @@
 package ufrn.br.dto;
-import lombok.*;
-import org.springframework.hateoas.RepresentationModel;
-import ufrn.br.controller.CategoriaController;
-import ufrn.br.controller.ProdutoController;
-import ufrn.br.model.Categoria;
-import ufrn.br.model.Produto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProdutoRequestDTO extends RepresentationModel<ProdutoResponseDTO> {
+public class ProdutoRequestDTO {
     String nome;
     String descricao;
     float preco;
     int quantidade;
     List<CategoriaResponseDTO> categoria;
 
-    public void addLinks(Produto produto) {
-        this.add(linkTo(ProdutoController.class).slash(produto.getId()).withSelfRel());
 
-        if(produto.getCategoria() != null){
-            for(Categoria categoria : produto.getCategoria()){
-                this.add(linkTo(CategoriaController.class).slash(categoria.getId()).withRel("categoria"));
-            }
-        }
-    }
 }
