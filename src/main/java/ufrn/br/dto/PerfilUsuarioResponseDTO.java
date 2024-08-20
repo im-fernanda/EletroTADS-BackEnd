3b0791d4ffc90128a1e8505c0841abbd39bec6c1
Fernanda <gilneidefernandaf@gmail.com>
2024-08-20 16:23:36 -0300

    Revisar perfilUsuarioDTO

--- src/main/java/ufrn/br/dto/PerfilUsuarioResponseDTO.java
+++ src/main/java/ufrn/br/dto/PerfilUsuarioResponseDTO.java
@@ -2,22 +2,26 @@ package ufrn.br.dto;
 
 import lombok.Data;
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
 import ufrn.br.controller.EnderecoController;
 import ufrn.br.controller.PerfilUsuarioController;
 import ufrn.br.controller.UsuarioController;
 import ufrn.br.model.Endereco;
 import ufrn.br.model.PerfilUsuario;
+import ufrn.br.model.Usuario;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Data
-public class PerfilUsuarioResponseDTO {
+public class PerfilUsuarioResponseDTO extends RepresentationModel<PerfilUsuarioResponseDTO> {
     String nome;
     String genero;
     String dataNascimento;
 
-    public void addLinks(PerfilUsuario perfilUsuario) {
-        this.add(linkTo(PerfilUsuarioController.class).slash(perfilUsuario.getId()).withSelfRel());
+    public void addLinks(PerfilUsuario perfilUser){
+        this.add(linkTo(PerfilUsuarioController.class).slash(perfilUser.getId()).withSelfRel());
+        this.add(linkTo(UsuarioController.class).slash(perfilUser.getUsuario().getId()).withRel("usuario"));
+
     }
 
 }
