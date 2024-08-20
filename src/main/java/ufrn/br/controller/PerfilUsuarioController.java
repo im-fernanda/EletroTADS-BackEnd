479abb9daab319a995189436934af09a96350ec2
Fernanda <gilneidefernandaf@gmail.com>
2024-08-20 16:14:37 -0300

    perfilUsuarioController funcionando

--- src/main/java/ufrn/br/controller/PerfilUsuarioController.java
+++ src/main/java/ufrn/br/controller/PerfilUsuarioController.java
@@ -1,7 +1,12 @@
 package ufrn.br.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import ufrn.br.dto.PerfilUsuarioRequestDTO;
 import ufrn.br.dto.PerfilUsuarioResponseDTO;
+import ufrn.br.dto.UsuarioRequestDTO;
+import ufrn.br.dto.UsuarioResponseDTO;
+import ufrn.br.model.Endereco;
 import ufrn.br.model.PerfilUsuario;
 import ufrn.br.model.Usuario;
 import ufrn.br.service.PerfilUsuarioService;
@@ -26,6 +31,62 @@ import static java.util.stream.Collectors.toList;
 public class PerfilUsuarioController {
     private final PerfilUsuarioService service;
     private final ModelMapper mapper;
-    
-    
+
+    @GetMapping
+    public Page<PerfilUsuarioResponseDTO> listAll(Pageable pageable) {
+        Page<PerfilUsuario> perfilUsuariosPage = service.listAll(pageable);
+        return perfilUsuariosPage.map(this::convertToDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PerfilUsuarioResponseDTO> create(@RequestBody PerfilUsuarioRequestDTO userDto){
+        Usuario created = service.create(convertToEntity(userDto));
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(convertToDto(created));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<UsuarioResponseDTO> getById(@PathVariable("id") Long id){
+        PerfilUsuario perfilUsuario = service.findById(id);
+        UsuarioResponseDTO perfilUsuarioDto = mapper.map(perfilUsuario, UsuarioResponseDTO.class);
+
+        return ResponseEntity.ok(perfilUsuarioDto);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id){
+        service.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<PerfilUsuarioResponseDTO> update(@RequestBody PerfilUsuarioRequestDTO perfilUserDto, @PathVariable("id") Long id){
+        try {
+            PerfilUsuario perfilUsuario = service.findById(id);
+        } catch (Exception e) {
+            return this.create(perfilUserDto);
+        }
+
+        PerfilUsuario perfilUsuarioUpdated = service.update(mapper.map(perfilUserDto, PerfilUsuario.class), id);
+        return ResponseEntity.ok(convertToDto(perfilUsuarioUpdated));
+    }
+
+    private PerfilUsuarioResponseDTO convertToDto(PerfilUsuario perfilUsuarioCreated){
+        PerfilUsuarioResponseDTO perfilUsuarioResponseDto = mapper.map(perfilUsuarioCreated, PerfilUsuarioResponseDTO.class);
+        perfilUsuarioResponseDto.addLinks(perfilUsuarioCreated);
+
+        return perfilUsuarioResponseDto;
+    }
+
+    private PerfilUsuario convertToEntity(PerfilUsuarioRequestDTO perfilUserDto){
+        PerfilUsuario entityPerfilUsuario = mapper.map(perfilUserDto, PerfilUsuario.class);
+
+        return entityPerfilUsuario;
+    }
 }
--- src/main/java/ufrn/br/dto/PerfilUsuarioResponseDTO.java
+++ src/main/java/ufrn/br/dto/PerfilUsuarioResponseDTO.java
@@ -1,13 +1,23 @@
 package ufrn.br.dto;
 
 import lombok.Data;
+import org.springframework.hateoas.Link;
+import ufrn.br.controller.EnderecoController;
+import ufrn.br.controller.PerfilUsuarioController;
+import ufrn.br.controller.UsuarioController;
+import ufrn.br.model.Endereco;
+import ufrn.br.model.PerfilUsuario;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Data
 public class PerfilUsuarioResponseDTO {
+    String nome;
+    String genero;
+    String dataNascimento;
 
-    private Long id;
-    private String nome;
-    private String genero;
-    private String dataNascimento;
+    public void addLinks(PerfilUsuario perfilUsuario) {
+        this.add(linkTo(PerfilUsuarioController.class).slash(perfilUsuario.getId()).withSelfRel());
+    }
 
 }
