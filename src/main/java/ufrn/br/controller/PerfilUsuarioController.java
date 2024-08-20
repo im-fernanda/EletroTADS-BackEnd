bb030e2bb1710797044b32b42ee17d39a9900187
Fernanda Felix <118316479+im-fernanda@users.noreply.github.com>
2024-08-20 15:17:43 -0300

    Iniciando o PerfilUsuarioController.java

--- src/main/java/ufrn/br/controller/PerfilUsuarioController.java
+++ src/main/java/ufrn/br/controller/PerfilUsuarioController.java
@@ -1,25 +1,31 @@
 package ufrn.br.controller;
 
+import ufrn.br.dto.PerfilUsuarioRequestDTO;
+import ufrn.br.dto.PerfilUsuarioResponseDTO;
 import ufrn.br.model.PerfilUsuario;
+import ufrn.br.model.Usuario;
 import ufrn.br.service.PerfilUsuarioService;
 import ufrn.br.service.UsuarioService;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
-@RequestMapping("/perfil")
+@RequestMapping("/perfil/")
 @AllArgsConstructor
 public class PerfilUsuarioController {
-
-    PerfilUsuarioService service;
-
-    @GetMapping
-    public PerfilUsuario getById(@PathVariable Long id_usuario) {
-        return service.findById(id_usuario);
-    }
+    private final PerfilUsuarioService service;
+    private final ModelMapper mapper;
+    
+    
 }
